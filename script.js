7ccca48cb056d26487914b322e5f70127fd6e73c
Rafael <you@example.com>
2022-01-26 16:23:18 +0400

    html

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const langW = document.querySelector(".langWrap");
+const link = document.querySelectorAll("a");
+const about = document.querySelector(".about");
+const education = document.querySelector(".education");
+const skils = document.querySelector(".skils");
+const gallery = document.querySelector(".gallery");
