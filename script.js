303a17b9a4525a3f5da7795c7d5fb79dd0e0f1b8
Rafael <you@example.com>
2022-01-28 13:21:53 +0400

    html

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
 const langW = document.querySelector(".langWrap");
 const link = document.querySelectorAll("a");
-const about = document.querySelector(".about");
-const education = document.querySelector(".education");
-const skils = document.querySelector(".skils");
-const gallery = document.querySelector(".gallery");
+const aboutv = document.querySelector(".about");
+const educationv = document.querySelector(".education");
+const skilsv = document.querySelector(".skils");
+const galleryv = document.querySelector(".gallery");
 
 link.forEach(el => {
     el.addEventListener('click' , () => {
-        const attr = el.getAttribute('language')
+        const attr = el.getAttribute('language');
+
+        
+        aboutv.textContent = data[attr].about;
+        educationv.textContent = data[attr].education;
+        skilsv.textContent = data[attr].skils;
+        galleryv.textContent = data[attr].gallery;
     })
 })
+
+var data = {
+    "armenia": {
+        "about": "Մեր Մասին",
+        "education": "Կրթություն",
+        "skils": "Հմտություններ",
+        "gallery": "Նկարներ",
+    },
+
+    "russia": {
+        "about": "о нас",
+        "education": "образование",
+        "skils": "навыки",
+        "gallery": "галерея",
+    },
+
+    "america": {
+        "about": "about",
+        "education": "education",
+        "skils": "skills",
+        "gallery": "gallery",
+    },
+}
